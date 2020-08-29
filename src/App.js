98dd14e1795459bc6ae2979bcd182fbccffe7af3
Sundram Mishra <newsundram2018@gmail.com>
2020-08-29 18:29:04 +0530

    pipline created and pushed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           My Awesome React App Sundram Mishra
+          First Line.
         </p>
       </header>
     </div>
